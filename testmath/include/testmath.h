bcc2536c37e3ca0aca26b6c3e6a9dbacd11f0870
AndrewRademacher <andrewrademacher@icloud.com>
2018-02-26 14:42:45 -0600

    Added testmath library.

---
+++ testmath/include/testmath.h
@@ -0,0 +1,10 @@
+#ifndef TESTMATH_H
+#define TESTMATH_H
+
+namespace testmath {
+
+    double multiply(double a, double b);
+
+}
+
+#endif
---
+++ testmath/src/testmath.cpp
@@ -0,0 +1,9 @@
+#include "testmath.h"
+
+namespace testmath {
+
+    double multiply(double a, double b) {
+        return a * b;
+    }
+
+}
