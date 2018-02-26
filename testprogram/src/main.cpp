ccc5629538310fd05976ef7902de48910f6acff3
AndrewRademacher <andrewrademacher@icloud.com>
2018-02-26 16:07:09 -0600

    Complete monolithic installer.

--- testprogram/src/main.cpp
+++ testprogram/src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
 
+#include <testmath.h>
+
 int main(int argc, char** argv) {
     std::cout << "Hello World!" << std::endl;
+    std::cout << "3 * 4 = " << testmath::multiply(3, 4) << std::endl;
 
     return EXIT_SUCCESS;
 }
